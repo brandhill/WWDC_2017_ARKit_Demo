6e0a7590fe0eca07f38b4971a3c503d05943a086
Hill Hung <brandhill.g@gmail.com>
2017-08-21 17:22:14 +0800

    apply syntax changed

--- ARKitExample/TextManager.swift
+++ ARKitExample/TextManager.swift
@@ -136,7 +136,9 @@ class TextManager {
 				switch reason {
 				case .excessiveMotion: message += "Try slowing down your movement, or reset the session."
 				case .insufficientFeatures: message += "Try pointing at a flat surface, or reset the session."
-				}
+                case .initializing:
+                    message += "Try initializing the session."
+                }
 			case .normal: break
 			}
 			
